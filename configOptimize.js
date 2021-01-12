3ecd4f7b69f5d1dca96e5fb404b9e6eb2a16c552
meohyun2 <eogus6242@gmail.com>
2021-01-13 01:21:41 +0900

    first commit

---
+++ configOptimize.js
@@ -0,0 +1,7 @@
+import * as optimizelyReactSDK from '@optimizely/react-sdk';
+
+// Instantiate an Optimizely client
+const optimizely = optimizelyReactSDK.createInstance({
+  datafile: optimizelyDatafile,
+})
+
